56e54a9e8027172c5f6e465523ccba00c5b753b8
ARTEM BOYKO <boyko.a.v@bk.ru>
2024-02-20 12:11:09 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const hello = ref('Привет накст!')
+</script>
+
+<template>
+    <NuxtLoadingIndicator />
+    <NuxtPage />
+  <div>
+    <h1>{{ hello }}</h1>
+  </div>
+</template>
+<style>
+
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,38 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+  app: {
+    head: {
+      meta: [
+        // <meta name="viewport" content="width=device-width, initial-scale=1">
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      script: [
+        // <script src="https://myawesome-lib.js"></script>
+        { src: 'https://awesome-lib.js' }
+      ],
+      link: [
+        // <link rel="stylesheet" href="https://myawesome-lib.css">
+        { rel: 'stylesheet', href: 'https://awesome-lib.css' }
+      ],
+      // please note that this is an area that is likely to change
+      style: [
+        // <style type="text/css">:root { color: red }</style>
+        { children: ':root { color: red }', type: 'text/css' }
+      ],
+      noscript: [
+        // <noscript>JavaScript is required</noscript>
+        { children: 'JavaScript is required' }
+      ]
+    }
+  },
+  css: [
+    // Load a Node.js module directly (here it's a Sass file).
+    // 'bulma',
+    // CSS file in the project
+    '~/assets/css/main.css',
+    // SCSS file in the project
+    '~/assets/css/main.scss'
+  ],
+ modules: ['@pinia/nuxt'],
+})
